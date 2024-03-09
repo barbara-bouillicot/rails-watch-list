efc79b8cd6812e62d646b998dc69e82cc28e02d7
barbara bouillicot <barbara.bouillicot@hotmail.fr>
2024-03-09 09:32:59 +0100

    review feature added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :reviews, only: [:new, :create]
     resources :bookmarks, only: [:index, :new, :create]
   end
   resources :bookmarks, only: [:destroy]
